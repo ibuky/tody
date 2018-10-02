36c104b5e6c16ce8db52ed51be474c11cd32e12e
ibuky@VTX0 <ivuki.maxx@gmail.com>
2018-10-03 00:41:58 +0900

    daily commit

--- www/js/index.js
+++ www/js/index.js
@@ -28,8 +28,6 @@ document.addEventListener('deviceready', function(event) {
  * テーブルを作成します。
  */
 function createTable() {
-    var db = null;  // DBコネクション保持用
-
     // DBコネクションを取得
     db = window.sqlitePlugin.openDatabase({
         name     : 'todolist.db',
@@ -65,4 +63,11 @@ function createTable() {
         // SQL処理成功時
         console.log('テーブル初期化成功');
     });
+
+    // SQL(SELECT)を実行
+    db.transaction(function(tx) {
+        tx.executeSql('SELECT * FROM todo LIMIT 100', [], function(tx, result) {
+            console.log(result.rows);
+        })
+    })
 }
--- www/js/register.js
+++ www/js/register.js
@@ -1,12 +1,15 @@
 var js_register = {
-    
+    /* 定数 */
+    FLAG_Y : '1',  // フラグ(true)
+    FLAG_N : '0',  // フラグ(false)
+
     /**
      * ページ表示時の処理
      */
     init : function(page) {
         // 登録ボタン
-        var button = document.getElementById('submit');
-        button.addEventListener('click', this.onClickSubmitButton.bind(this), false);
+        var submit_button    = document.getElementById('submit');
+        submit_button.addEventListener('click', this.onClickSubmitButton.bind(this), false);
     },
 
     /**
@@ -18,49 +21,53 @@ var js_register = {
             return;
         }
 
-        var input_data = this.getInputData();                // 入力情報の取得
-        var sql_insert = this.createInsertSql(input_data);   // INSERT文の作成
+        var input_data = this.getInputData();   // 入力情報の取得
 
-        if (this.execInsertSql(sql_insert)) {   // INSERT文の実行とともに、実行結果を評価
-            /***** INSERT成功時 *****/
-            this.showNoTitlePopup('登録しました！');
-
-            // 画面遷移
-            document.getElementById('navi').resetToPage('list.html');
-
-        } else {
-            /***** INSERT失敗時 *****/
-            this.showNoTitlePopup('登録に失敗しました...');
-        }
+        // INSERT文の実行(Promise)
+        this.execInsertSql(input_data)
+            .then(function() {
+                // INSERT成功時
+                this.showNoTitlePopup('登録しました！');
+                document.getElementById('navi').resetToPage('list.html');   // 一覧画面に戻る
+            }.bind(this))
+            .catch(function() {
+                // INSERT失敗時
+                this.showNoTitlePopup('登録に失敗しました...');
+            }.bind(this));
     },
 
     /**
      * 入力情報のチェックを行います。
+     * @returns {boolean} エラーが発生していない場合、true
      */
     checkInputData : function() {
         // タイトルのチェック
-        var title = document.getElementById('title');
-        if (title.value === '') {
+        var title_val = document.getElementById('title').value;
+        if (!title_val) {
             // 空の場合、エラー
             this.showNoTitlePopup('タイトルを入力してください。');
             return false;
         }
 
-        if (title.length > 50) {
+        if (title_val.length > 50) {
             // 50文字以上の場合、エラー
             this.showNoTitlePopup('タイトルは50字以内で入力してください。');
             return false;
         }
 
         // 詳細情報のチェック
-        var detail = document.getElementById('detail');
-        var detail_val = detail.value;
+        var detail_val = document.getElementById('detail').value;
         
-        if (detail_val !== '') {
+        if (detail_val) {
             // 詳細情報に入力がある場合
-
-            var detail_line_count = detail.value.match(/\n/g).length + 1;   // 行数を取得
-
+            if (!detail_val.match(/\n/g)) {
+                // 改行コードが存在していない場合(1行)
+                return true;
+            }
+            
+            // 改行コードが存在している場合(2行以上)
+            var detail_line_count = detail_val.match(/\n/g).length + 1;
+            
             // 行数チェック
             if (detail_line_count > 5) {
                 // 6行以上の入力の場合、エラー
@@ -76,25 +83,24 @@ var js_register = {
     getInputData : function() {
         var input_vals = [];    // 入力値保持用
         
-        input_vals.title     = document.getElementById('title').value;        // タイトル
-        input_vals.detail    = document.getElementById('detail').value;       // 詳細情報
-        input_vals.important = document.getElementById('important');    // 重要フラグ
-        input_vals.urgent    = document.getElementById('urgent');       // 緊急フラグ
+        input_vals.title     = document.getElementById('title').value;  // タイトル
+        input_vals.detail    = document.getElementById('detail').value; // 詳細情報
+        input_vals.important = this.getYnValueForSwitch(document.getElementById('important').checked);    // 重要フラグ
+        input_vals.urgent    = this.getYnValueForSwitch(document.getElementById('urgent').checked);       // 緊急フラグ
         
-
         return input_vals;
     },
 
     /**
      * スイッチのON/OFF状態を判定し、y/nの値を返します。
      * @param {boolean} val switch要素のchecked
-     * @returns {string} スイッチがONの場合、'y'
+     * @returns {string} スイッチがONの場合、1(true)
      */
     getYnValueForSwitch : function(val) {
         if (val) {
-            return 'y'; // スイッチがONの場合
+            return this.FLAG_Y; // スイッチがONの場合
         } else {
-            return 'n'; // スイッチがOFFの場合
+            return this.FLAG_N; // スイッチがOFFの場合
         }
     },
 
@@ -107,5 +113,37 @@ var js_register = {
             title : '',
             message : message,
         })
-    }
+    },
+
+    /**
+     * 入力した情報からINSERT文を発行します。
+     * @param {object} input_data 入力情報
+     * @returns {Promise} INSERTに成功した場合、then 
+     */
+    execInsertSql : function(input_data) {
+        // INSERT文の実行
+        return new Promise(function(resolve, reject) {
+            db.transaction(function(tx) {
+                var insert_sql = 
+                      'INSERT INTO todo (valid, title, detail, important, urgent, date) '
+                    + 'VALUES (1,?,?,?,?,CURRENT_TIMESTAMP)';   // INSERT文
+
+                tx.executeSql(
+                    insert_sql,
+                    [input_data.title, input_data.detail, input_data.important, input_data.urgent]
+                );
+            },
+            function(error) {
+                // INSERT失敗時
+                console.log('INSERTに失敗しました : ' + error.message);
+                reject();
+            },
+            function() {
+                // INSERT成功時
+                console.log('INSERTに成功しました');
+                console.log(input_data);
+                resolve();
+            });
+        });
+    },
 }
